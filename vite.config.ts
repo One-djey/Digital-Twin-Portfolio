8514671ed234792788d33e6882ce1bcccf2ebefa
One-djey <grm.83600@gmail.com>
2025-02-20 00:04:28 +0100

    fix vercel deployment #5

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-export default defineConfig({
+const config = {
   plugins: [react(), runtimeErrorOverlay(), themePlugin()],
   resolve: {
     alias: {
@@ -17,7 +17,8 @@ export default defineConfig({
   },
   root: path.resolve(__dirname, "client"),
   build: {
-    outDir: path.resolve(__dirname, "dist/public"),
+    outDir: path.resolve(__dirname, "dist/client"),
     emptyOutDir: true,
   },
-});
+}
+export default defineConfig(config);
